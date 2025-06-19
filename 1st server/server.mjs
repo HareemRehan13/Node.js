5b3a7af4899acd4b696eecd3cb8fc9fec4adcd9b
hareem13 <hareemrehan07@gmail.com>
2025-06-19 19:40:03 +0500

    crud

--- 1st server/server.mjs
+++ 1st server/server.mjs
@@ -2,18 +2,23 @@
 
 import express from 'express'
 import fs from 'node:fs'
-import { json } from 'node:stream/consumers'    
 
 const app = express()
 const port = 3000
 const data = JSON.parse(fs.readFileSync('data.json','utf-8'));
-console.log(data);
+// console.log(data);
+
 let products = data.products;
+//Middleware
+//Body Parser
+app.use(express.json())
+
 app.get('/', (req, res) => {
 //   res.send('Hello World!')
 res.json(products).status(200)
 })
 
+//Rest API's
 //Route Parameter
 app.get('/product/:id', (req, res) => {
     const id = req.params.id;
@@ -30,12 +35,43 @@ app.get('/product/:id', (req, res) => {
     else{
 res.status(404).json({message:"Product not found"})
     }
-//   res.json({title:'Apple Watch'})
 })
 
 app.get('/test', (req, res) => {
   res.json({title:'Testing live server'})
 })
+
+
+//Add  , Edit & Delete
+
+app.post('/addProduct', (req, res) => {
+const product ={
+id: req.body.id,
+title: req.body.title,
+description: req.body.description,
+price: req.body.rice,
+stock: req.body.stock,
+}
+console.log(product);
+products = [...products,product]
+
+ res.json({message:"Product added successfully", 
+  productAdded:product});
+ 
+})
+
+app.delete('/deleteProduct/:id', (req, res) => {
+const id = req.params.id;
+products = products.filter((item,index) =>{
+if(item.id != id){
+return item;
+}
+})
+ res.json({message:"Product deleted successfully", 
+});
+ 
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
